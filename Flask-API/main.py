e581f300d6c00b1324bea2220a656e26fe2021d7
mszulcc <mszulc003@gmail.com>
2025-04-27 16:56:21 +0200

    Added class for Time series management

--- Flask-API/main.py
+++ Flask-API/main.py
@@ -5,11 +5,28 @@ import sys
 from flask import Flask, jsonify, request
 import datetime
 
+
+class TimeSeriesManager:
+    def __init__(self):
+        self.timeseries = []
+
+    def add_timeseries(self, data):
+        if isinstance(data, list):
+            self.timeseries.extend(data)
+            return data
+        return None
+
+    def get_timeseries(self):
+        return self.timeseries
+
+    def clear_timeseries(self):
+        self.timeseries.clear()
+
 sys.stdout.reconfigure(line_buffering=True)
 
+timeseries_manager = TimeSeriesManager()
 app = Flask(__name__)
 
-timeseries = []
 
 
 
@@ -23,24 +40,24 @@ timeseries = []
 
 @app.route("/timeseries", methods=["GET"])
 def get_timeseries():
-    data = timeseries.copy()
-    timeseries.clear()
-    return jsonify({"timeseries1": data}), 201
+    timeseries_data = timeseries_manager.get_timeseries()
+    return jsonify({"timeseries1": timeseries_data}), 201
 
 # , "timeseries2": timeseries2
 
 @app.route("/upload-timeseries", methods=["POST"])
 def add_timeseries():
-    global timeseries
     data = request.get_json()
     if not data:
         return jsonify({"error": "Invalid request"}), 400
+    timeseries_manager.clear_timeseries()
+    added_data = timeseries_manager.add_timeseries(data)
 
 
     if isinstance(data, list):
-        timeseries.extend(data)
-    return jsonify(data), 201
-    # else:
+        added_data.extend(data)
+    return jsonify(added_data), 201
+    #
     #     return jsonify({
     #         "error": "Invalid data format",
     #         "expected": "Array of entries or object with 'log_date' field"
