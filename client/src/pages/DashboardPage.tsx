f49cd14394da4bf3399a23c5505066ff31229b61
Karol Kowalczyk <karolajoj@gmail.com>
2025-05-15 21:12:43 +0200

    Add separate page components for navigation routes

---
+++ client/src/pages/DashboardPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DashboardPage: React.FC = () => {
+  return <h1>This is Dashboard Page</h1>;
+};
+
+export default DashboardPage;
---
+++ client/src/pages/PluginsPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PluginsPage: React.FC = () => {
+  return <h1>This is Plugins Page</h1>;
+};
+
+export default PluginsPage;
